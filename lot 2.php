738ed71f92dd65ad28a551cb135cd547ee64351c
11sf21 <11sf21@queensu.ca>
2015-03-29 18:20:33 -0400

    Update lot 2.php

--- lot 2.php
+++ lot 2.php
@@ -1,4 +1,22 @@
 <html>
+  <head>
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  
+  <body>
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="index.html">KTCS Home</a></li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="memberEntry.html">Register</a></li>
+          <li><a href="memberLinks.html">Member</a></li>
+          <li><a href="administratorLinks.html">Administrator</a></li>
+        </ul>
+      </div>
+    </div>
   <?php
     require_once 'login.php';
 	  $conn = new mysqli($hn, $un, $pw, $db);
@@ -16,5 +34,4 @@
     $result->close();
     $conn->close();
   ?>
-  <a href="administratorLinks.html">Return</a> 
 </html>
