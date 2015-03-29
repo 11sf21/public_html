382f335a9c0cf464d5c5e6630bd86fb3d970ccf1
11sf21 <11sf21@queensu.ca>
2015-03-29 15:57:47 -0400

    Create queryLocations.php

---
+++ queryLocations.php
@@ -0,0 +1,21 @@
+<html>
+  <?php
+    require_once 'login.php';
+	  $conn = new mysqli($hn, $un, $pw, $db);
+	  if ($conn->connect_error) die($conn->connect_error);
+	
+	  $result = mysqli_query($conn, "SELECT * FROM locations")
+      or die ("Couldn't execute query.");
+           
+	  while($row = mysqli_fetch_assoc($result))
+    {
+      extract($row);
+      echo <<<_END
+      <a href="$location_address.html">$location_address</a>
+    }
+  
+    $result->close();
+    $conn->close();
+  ?>
+  <a href="administratorLinks.html">Return</a> 
+</html>
