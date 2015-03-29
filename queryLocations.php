060133464d64e45363f2c7e3b6509829fd0f002c
11sf21 <11sf21@queensu.ca>
2015-03-29 16:00:59 -0400

    Update queryLocations.php

--- queryLocations.php
+++ queryLocations.php
@@ -1,21 +1,22 @@
 <html>
   <?php
     require_once 'login.php';
-	  $conn = new mysqli($hn, $un, $pw, $db);
-	  if ($conn->connect_error) die($conn->connect_error);
+      $conn = new mysqli($hn, $un, $pw, $db);
+	    if ($conn->connect_error) die($conn->connect_error);
 	
-	  $result = mysqli_query($conn, "SELECT * FROM locations")
+	    $result = mysqli_query($conn, "SELECT * FROM locations")
       or die ("Couldn't execute query.");
            
-	  while($row = mysqli_fetch_assoc($result))
-    {
-      extract($row);
-      echo <<<_END
-      <a href="$location_address.html">$location_address</a>
-    }
+	    while($row = mysqli_fetch_assoc($result))
+      {
+        extract($row);
+        echo <<<_END
+        <a href="$location_address.html">$location_address</a>
+        _END;
+      }
   
-    $result->close();
-    $conn->close();
+      $result->close();
+      $conn->close();
   ?>
   <a href="administratorLinks.html">Return</a> 
 </html>
