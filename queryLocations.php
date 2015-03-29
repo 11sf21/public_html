aad425542914ed21b9bec201ba66c254ea66013b
11sf21 <11sf21@queensu.ca>
2015-03-29 18:22:25 -0400

    Update queryLocations.php

--- queryLocations.php
+++ queryLocations.php
@@ -1,4 +1,22 @@
 <html>
+  <head>
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  
+  <body>
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="index.html">KTCS Home</a></li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="memberEntry.html">Register</a></li>
+          <li><a href="memberLinks.html">Member</a></li>
+          <li><a href="administratorLinks.html">Administrator</a></li>
+        </ul>
+      </div>
+    </div>
   <?php
     require_once 'login.php';
       $conn = new mysqli($hn, $un, $pw, $db);
@@ -11,13 +29,12 @@
       {
         extract($row);
         //echo "$location_address<br>";
-	$test = "$location_address.php";
-        echo '<a href="' . $test. '">' . $location_address . '</a><br>';
+	      $lot = "$location_address.php";
+        echo '<a href="' . $lot. '">' . $location_address . '</a><br>';
 
       }
   
       $result->close();
       $conn->close();
   ?>
-  <a href="administratorLinks.html">Return</a> 
 </html>
