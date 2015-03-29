3f430102a83a64c723e88f0beccafa0eb2088a64
11sf21 <11sf21@queensu.ca>
2015-03-29 16:13:11 -0400

    Create lot 1.php

---
+++ lot 1.php
@@ -0,0 +1,20 @@
+<html>
+  <?php
+    require_once 'login.php';
+	  $conn = new mysqli($hn, $un, $pw, $db);
+	  if ($conn->connect_error) die($conn->connect_error);
+	
+	  $result = mysqli_query($conn, "SELECT * FROM car")
+      or die ("Couldn't execute query.");
+           
+	  while($row = mysqli_fetch_assoc($result))
+    {
+      extract($row);
+      echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
+    }
+  
+    $result->close();
+    $conn->close();
+  ?>
+  <a href="administratorLinks.html">Return</a> 
+</html>
