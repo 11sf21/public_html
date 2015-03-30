3e0b3bd70bc63352001baf39b2105e9a45b4ca31
11sf21 <11sf21@queensu.ca>
2015-03-30 16:00:51 -0400

    Update maint5000.php
    trying table

--- maint5000.php
+++ maint5000.php
@@ -1,4 +1,14 @@
 <html>
+	<table style="border-collapse:collapse;">
+	   <tr>
+	   <th style="padding:5px;">VIN</th>
+		 <th style="padding:5px;border-left:1px solid black;">Make</th>
+		 <th style="padding:5px;border-left:1px solid black;">Model</th>
+		 <th style="padding:5px;border-left:1px solid black;">Year</th>
+		 <th style="padding:5px;border-left:1px solid black;">Current status</th>
+		 <th style="padding:5px;border-left:1px solid black;">Current status</th>
+		 <th style="padding:5px;border-left:1px solid black;">Current status</th>
+		 <th style="padding:5px;border-left:1px solid black;">Current status</th>
   <?php
     require_once 'login.php';
 	  $conn = new mysqli($hn, $un, $pw, $db);
@@ -10,11 +20,19 @@
 	  while($row = mysqli_fetch_assoc($result))
     {
       extract($row);
-      echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
+      echo '<tr style="border-top:1px solid black;"><td style="padding:5px;border-top:1px solid black;">'.$vin.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$make.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$model.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$year.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$current_status.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$last_odometer_reading.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$last_maint_odometer.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td></tr>';
+      //echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
     }
   
     $result->close();
     $conn->close();
   ?>
-  <a href="administratorLinks.html">Return</a> 
+  </table>
 </html>
