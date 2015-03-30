fa7cb0962dd55c8160dbc19ba954e3c29ded3ea9
11sf21 <11sf21@queensu.ca>
2015-03-30 16:14:41 -0400

    Update maint5000.php

--- admin/maint5000.php
+++ admin/maint5000.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
 <html>
-  <head>
-    <link href="./css/bootstrap.css" rel="stylesheet"> 
-    <link rel="stylesheet" href="./main.css"> 
-  </head>
-  
-  <body>
-    <div class="nav">
-      <div class="container">
-        <ul class="pull-left">
-          <li><a href="../index.html">KTCS Home</a></li>
-        </ul>
-        <ul class="pull-right">
-          <li><a href="../memberEntry.html">Register</a></li>
-          <li><a href="../member/memberLinks.html">Member</a></li>
-          <li><a href="administratorLinks.html">Administrator</a></li>
-        </ul>
-      </div>
-    </div>
+	<table style="border-collapse:collapse;">
+	   <tr>
+	   <th style="padding:5px;">VIN</th>
+		 <th style="padding:5px;border-left:1px solid black;">Make</th>
+		 <th style="padding:5px;border-left:1px solid black;">Model</th>
+		 <th style="padding:5px;border-left:1px solid black;">Year</th>
+		 <th style="padding:5px;border-left:1px solid black;">Current status</th>
+		 <th style="padding:5px;border-left:1px solid black;">Last Odometer</th>
+		 <th style="padding:5px;border-left:1px solid black;">Last Maint Odometer</th>
+		 <th style="padding:5px;border-left:1px solid black;">Location Address</th>
   <?php
     require_once 'login.php';
 	  $conn = new mysqli($hn, $un, $pw, $db);
@@ -29,12 +20,19 @@
 	  while($row = mysqli_fetch_assoc($result))
     {
       extract($row);
-      echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
+      echo '<tr style="border-top:1px solid black;"><td style="padding:5px;border-top:1px solid black;">'.$vin.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$make.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$model.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$year.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$current_status.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$last_odometer_reading.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$last_maint_odometer.'</td>';
+			echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td></tr>';
+      //echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
     }
   
     $result->close();
     $conn->close();
   ?>
-    <a href="administratorLinks.html">Return</a> 
-  </body>
+  </table>
 </html>
