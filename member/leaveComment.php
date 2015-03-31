7b44abd11e8ba21b409e12b9d9722a0aa162734b
Steven Fry <11sf21@queensu.ca>
2015-03-31 11:20:28 -0400

    comments member

--- member/leaveComment.php
+++ member/leaveComment.php
@@ -1,48 +1,28 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>K-Town Car Share Registration</title>
-</head>
-<body>
-  <h2>K-Town Car Share Registration</h2>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Leave a comment!</title>
+  </head>
+  <body>
 
-<?php
-  require_once 'login.php';
+    <?php
+      require_once 'login.php';
   
-  $name = $_POST['name'];
-  $address = $_POST['address'];
-  $phonenumber = $_POST['phonenumber'];
-  $email = $_POST['email'];
-  $driverlicnumber = $_POST['driverlicnumber'];
-  $creditcardnumber = $_POST['creditcardnumber'];
-  $creditcardexpiry = $_POST['creditcardexpiry'];
-  $date = date("Y-m-d");
+      $member_number = $_POST['member_number'];
+      $topic = $_POST['topic'];
+      $comment = $_POST['comment'];
 
-  $dbc = mysqli_connect($hn, $un, $pw, $db)
-    or die('Error connecting to MySQL server.');
+      $dbc = mysqli_connect($hn, $un, $pw, $db)
+      or die('Error connecting to MySQL server.');
 
-  $query = "INSERT INTO member (member_number, name, address,phone_number, " .
-    "email, driver_lic_number, credit_card_number, credit_card_expiry, registration_date) " .
-    "VALUES (NULL, '$name', '$address', '$phonenumber', '$email', '$driverlicnumber', " .
-    "'$creditcardnumber', '$creditcardexpiry', '$date')";
+      $query = "INSERT INTO comments (comment_number, member_number, topic, comment) " .
+      "VALUES (NULL, '$member_number', '$topic', '$comment')";
 
-  $result = mysqli_query($dbc, $query)
-    or die('Error querying database.');
+      $result = mysqli_query($dbc, $query)
+      or die('Error querying database.');
 	
-	$result = mysqli_query($dbc, "SELECT * FROM member WHERE name = '$name'")
-           or die ("Couldn't execute query.");
-	while($row = mysqli_fetch_assoc($result))
-      	{
-           extract($row);
-           echo "Your member number is $member_number<br />";
-        }
-        
-
-  mysqli_close($dbc);
-
-
-?>
-<a href="index.html">KTCS Home</a>
-</body>
+      mysqli_close($dbc);
+    ?>
+  </body>
 </html>
