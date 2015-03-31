b2629a98f22d45f068e877b740e9e68f4edc9e96
Steven Fry <11sf21@queensu.ca>
2015-03-31 12:09:31 -0400

    reserve

--- member/lotCars.php
+++ member/lotCars.php
@@ -13,9 +13,16 @@
           <li><a href="../index.html">KTCS Home</a></li>
         </ul>
         <ul class="pull-right">
-          <li><a href="../memberEntry.html">Display Car Rental History</a></li>
-          <li><a href="./memberLinks.html">Add Car to Location</a></li>
+          <li><a href="../memberEntry.html">Register</a></li>
+          <li><a href="./memberLinks.html">Member</a></li>
           // <li><a href="../admin/administratorLinks.html">Administrator</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-right">
+          <li><a href="reserveCar.html">Reserve Car</a></li>
         </ul>
       </div>
     </div>
@@ -52,7 +59,12 @@
         echo '<td style="padding:5px;border-left:1px solid black;">'.$current_status.'</td>';
         echo '<td style="padding:5px;border-left:1px solid black;">'.$last_odometer_reading.'</td>';
         echo '<td style="padding:5px;border-left:1px solid black;">'.$last_maint_odometer.'</td>';
-        echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td></tr>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td>';
+        echo '<form method="post" action="reserveCar.php">';
+        echo '<td style="padding:5px;border-left:1px solid black;"><button type="submit" value="'.$vin.'" name="vin">Reserve Car</button></td>';
+        echo '</form>';
+
+
       }
   
       $result->close();
