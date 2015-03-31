2c3b42d6e113ac477ba27b9a3b7d3a085302ae95
Steven Fry <11sf21@queensu.ca>
2015-03-31 11:57:25 -0400

    member view locations

---
+++ member/lotCars.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="../main.css">
+  </head>
+  
+  <body>
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="../index.html">KTCS Home</a></li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="../memberEntry.html">Display Car Rental History</a></li>
+          <li><a href="./memberLinks.html">Add Car to Location</a></li>
+          // <li><a href="../admin/administratorLinks.html">Administrator</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <table style="border-collapse:collapse;border:1px solid black;">
+    <th style="padding:5px;border-top:1px solid black;">VIN</th>
+    <th style="padding:5px;border-left:1px solid black;">Make</th>
+    <th style="padding:5px;border-left:1px solid black;">Model</th>
+    <th style="padding:5px;border-left:1px solid black;">Year</th>
+    <th style="padding:5px;border-left:1px solid black;">Current status</th>
+    <th style="padding:5px;border-left:1px solid black;">Last Odometer</th>
+    <th style="padding:5px;border-left:1px solid black;">Last Maint Odometer</th>
+    <th style="padding:5px;border-left:1px solid black;">Location Address</th>
+
+    <?php
+      $loc_address = $_POST["address"];
+      require_once 'login.php';
+      $conn = new mysqli($hn, $un, $pw, $db);
+      if ($conn->connect_error) die($conn->connect_error);
+  
+      $result = mysqli_query($conn, "SELECT * FROM car WHERE location_address = '$loc_address'")
+      or die ("Couldn't execute query.");
+           
+      while($row = mysqli_fetch_assoc($result))
+      {
+        extract($row);
+        //$test = "$location_address.php"; 
+        //echo '<a href="' . $test. '">' . $location_address . '</a><br>'; 
+        //echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
+        echo '<tr style="border-top:1px solid black;"><td style="padding:5px;border-top:1px solid black;">'.$vin.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$make.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$model.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$year.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$current_status.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$last_odometer_reading.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$last_maint_odometer.'</td>';
+        echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td></tr>';
+      }
+  
+      $result->close();
+      $conn->close();
+    ?>
+    </table>
+  </body>
+</html>
--- member/queryLocations.php
+++ member/queryLocations.php
@@ -1,40 +1,51 @@
 <html>
   <head>
-    <link href="./css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="main.css">
+    <link href="../css/bootstrap.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="../main.css"> 
   </head>
   
   <body>
     <div class="nav">
       <div class="container">
         <ul class="pull-left">
-          <li><a href="index.html">KTCS Home</a></li>
+          <li><a href="../index.html">KTCS Home</a></li>
         </ul>
         <ul class="pull-right">
-          <li><a href="memberEntry.html">Register</a></li>
-          <li><a href="memberLinks.html">Member</a></li>
-          <li><a href="administratorLinks.html">Administrator</a></li>
+          <li><a href="../memberEntry.html">Register</a></li>
+          <li><a href="./memberLinks.html">Member</a></li>
+          <li><a href="../admin/administratorLinks.html">Administrator</a></li>
         </ul>
       </div>
     </div>
-  <?php
-    require_once 'login.php';
+
+    <table style="border-collapse:collapse;border:1px solid black;">
+    <th style="padding:5px;border-top:1px solid black;">Address</th>
+    <th style="padding:5px;border-top:1px solid black;"></th>
+
+    <?php
+      require_once 'login.php';
       $conn = new mysqli($hn, $un, $pw, $db);
-	    if ($conn->connect_error) die($conn->connect_error);
-	
-	    $result = mysqli_query($conn, "SELECT * FROM locations")
+      if ($conn->connect_error) die($conn->connect_error);
+  
+      $result = mysqli_query($conn, "SELECT * FROM locations")
       or die ("Couldn't execute query.");
            
-	    while($row = mysqli_fetch_assoc($result))
+      while($row = mysqli_fetch_assoc($result))
       {
         extract($row);
         //echo "$location_address<br>";
-	      $lot = "$location_address.php";
-        echo '<h2><a href="' . $lot. '">' . $location_address . '</a></h2><br>';
+        //$lot = "$location_address.php";
+        //echo '<h2><a href="' . $lot. '">' . $location_address . '</a></h2><br>';
+        echo '<tr style="border-top:1px solid black;"><td style="padding:5px;">'.$location_address.'</td>';
+        echo '<form method="post" action="lotCars.php">';
+        echo '<td style="padding:5px;border-left:1px solid black;"><button type="submit" value="'.$location_address.'" name="address">View Cars</button></td>';
+        echo '</form>';
 
       }
   
       $result->close();
       $conn->close();
-  ?>
+    ?>
+    </table>
+  </body>
 </html>
