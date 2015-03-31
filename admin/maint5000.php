108acbe15dd947bdaff16a8e4c546390aecf2dea
Steven Fry <11sf21@queensu.ca>
2015-03-30 21:17:49 -0400

    table

--- admin/maint5000.php
+++ admin/maint5000.php
@@ -1,6 +1,24 @@
 <html>
-	<table style="border-collapse:collapse;">
-	   <tr style = "border-top:1px solid black;">
+  <head>
+    <link href="../css/bootstrap.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="../main.css"> 
+  </head>
+  
+  <body>
+    <div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="../index.html">KTCS Home</a></li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="../memberEntry.html">Register</a></li>
+          <li><a href="../member/memberLinks.html">Member</a></li>
+          <li><a href="administratorLinks.html">Administrator</a></li>
+        </ul>
+      </div>
+    </div>
+
+	<table style="border-collapse:collapse;border:1px solid black;">
 	   <th style="padding:5px;border-top:1px solid black;">VIN</th>
 		 <th style="padding:5px;border-left:1px solid black;">Make</th>
 		 <th style="padding:5px;border-left:1px solid black;">Model</th>
@@ -9,18 +27,18 @@
 		 <th style="padding:5px;border-left:1px solid black;">Last Odometer</th>
 		 <th style="padding:5px;border-left:1px solid black;">Last Maint Odometer</th>
 		 <th style="padding:5px;border-left:1px solid black;">Location Address</th>
-  <?php
-    require_once 'login.php';
-	  $conn = new mysqli($hn, $un, $pw, $db);
-	  if ($conn->connect_error) die($conn->connect_error);
+  	<?php
+    	require_once 'login.php';
+	  	$conn = new mysqli($hn, $un, $pw, $db);
+	  	if ($conn->connect_error) die($conn->connect_error);
 	
-	  $result = mysqli_query($conn, "SELECT * FROM car WHERE (last_odometer_reading - last_maint_odometer) > 5000")
-      or die ("Couldn't execute query.");
+	  	$result = mysqli_query($conn, "SELECT * FROM car WHERE (last_odometer_reading - last_maint_odometer) > 5000")
+      	or die ("Couldn't execute query.");
            
-	  while($row = mysqli_fetch_assoc($result))
-    {
-      extract($row);
-      echo '<tr style="border-top:1px solid black;"><td style="padding:5px;border-top:1px solid black;">'.$vin.'</td>';
+	  	while($row = mysqli_fetch_assoc($result))
+    	{
+      		extract($row);
+      		echo '<tr style="border-top:1px solid black;"><td style="padding:5px;border-top:1px solid black;">'.$vin.'</td>';
 			echo '<td style="padding:5px;border-left:1px solid black;">'.$make.'</td>';
 			echo '<td style="padding:5px;border-left:1px solid black;">'.$model.'</td>';
 			echo '<td style="padding:5px;border-left:1px solid black;">'.$year.'</td>';
@@ -29,10 +47,11 @@
 			echo '<td style="padding:5px;border-left:1px solid black;">'.$last_maint_odometer.'</td>';
 			echo '<td style="padding:5px;border-left:1px solid black;">'.$location_address.'</td></tr>';
       //echo "$vin,  $make,  $model,  $year,  $current_status,  $last_odometer_reading,  $last_maint_odometer,  $location_address<br />";
-    }
+    	}
   
     $result->close();
     $conn->close();
   ?>
   </table>
+  </body>
 </html>
